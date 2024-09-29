6b981775f5e1fdb83c9fda33e5dd156d9436e756
MRamazan <mramazantrn28@gmail.com>
2024-09-29 18:58:31 +0300

    Update parser.py

--- spvloc/spvloc/config/parser.py
+++ spvloc/spvloc/config/parser.py
@@ -3,7 +3,7 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image_path', type=str,help='path to image file',default="/home/godling/Downloads/deneme-main/layout_pred/example.jpg")
+    parser.add_argument('--image_path', type=str,help='path to image file',default="")
     parser.add_argument("-c", "--config_file", default="", type=str)
     parser.add_argument("-l", "--checkpoint_file", default="", type=str)
     parser.add_argument("-t", "--test_ckpt", default="", type=str)
